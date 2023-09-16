17ad6b900d81383ec191e1c01a5cdb6288d1e0dc
Muh Habibi <muhhabibi46@gmail.com>
2023-09-16 21:30:35 +0800

    done

--- RELAYtesting.ino
+++ RELAYtesting.ino
@@ -27,61 +27,81 @@ MAX6675 thermocouple (thermoCLK, thermoCS, thermoDO);
 #define relay2 D1
 #define relay3 D2
 #define relay4 D3
+#define relay5 D7
 
-// Ini syntax untuk otomatisasi relay valve dan relay water heater
-// BLYNK_WRITE(V0) { // Relay menyalakan air dan water heater secara otomatis
-//   bool value1 = param.asInt();
-//   if (value1 == 1) {
-//     digitalWrite(relay1, HIGH);
-//     Blynk.virtualWrite(V5, HIGH);
-//     delay(10000);
-//     digitalWrite(relay1, LOW);
-//     Blynk.virtualWrite(V5, LOW);
-//     // Blynk.virtualWrite(V0, LOW);
-//   }
-//   if(value1 == 1){
-//     digitalWrite(relay2, HIGH);
-//     Blynk.virtualWrite(V2, HIGH);
-//     Blynk.virtualWrite(V6, HIGH);
-//     delay(10000);
-//     digitalWrite(relay2, LOW);
-//     Blynk.virtualWrite(V0, LOW);
-//     Blynk.virtualWrite(V6, LOW);
-//   }
-// }
+BLYNK_WRITE(V2){
+  bool value2 = param.asInt();
+  if(value2 == 1){
+    digitalWrite(relay2, HIGH);
+    Blynk.virtualWrite(V6, HIGH);
+  }else{
+    digitalWrite(relay2, LOW);
+    Blynk.virtualWrite(V6, LOW);
+  }
+}
+
+BLYNK_WRITE(V3){
+  bool value3 = param.asInt();
+  if(value3 == 1){
+    digitalWrite(relay3, HIGH);
+    Blynk.virtualWrite(V7, HIGH);
+  }else{
+    digitalWrite(relay3, LOW);
+    Blynk.virtualWrite(V7, LOW);
+  }
+}
 
 BLYNK_WRITE(V0) {
   bool value1 = param.asInt();
+  bool value2 = param.asInt();
+  bool value3 = param.asInt();
   if (value1 == 1) {
-    digitalWrite(relay1, HIGH);
-    // Blynk.virtualWrite(V5, LOW);
-    // AKTIFKAN CODE DIBAWAH JIKA INGIN RELAY 1 MENGGUNAKAN DELAY
-    // delay(10000);
-    // digitalWrite(relay1, HIGH);
-    // Blynk.virtualWrite(V5, LOW);
-    // Blynk.virtualWrite(V0, LOW);
-  }else{
+    digitalWrite(relay1, HIGH); // relay air kran ke wadah air
+    Blynk.virtualWrite(V5, HIGH);
+    delay(5000); // waktunya hanya contoh dan bisa di ubah
     digitalWrite(relay1, LOW);
+    Blynk.virtualWrite(V0, LOW); // mengirim sinyal ke blynk agar sinkron
     Blynk.virtualWrite(V5, LOW);
   }
-}
-BLYNK_WRITE(V2) {
-  bool value2 = param.asInt();
-  if (value2 == 1) {
-    digitalWrite(relay2, HIGH);
-    delay(10000); // Butuh riset, berapa ML air bisa didapatkan jika dalam waktu per 1 sec. Takutnya airnya kebanyakan saat diolah krn dpt merusak adonan briket.
-    digitalWrite(relay2, LOW);
-    Blynk.virtualWrite(V2, LOW);
+  if(value1 == 1){
+    digitalWrite(relay2, HIGH); // relay water heater, relay akan OFF berdasarkan sensor suhu jika >= 80c
+    Blynk.virtualWrite(V6, HIGH);
+    Blynk.virtualWrite(V0, HIGH);
+    Blynk.logEvent("suhu", "Air sedang dipanaskan. Hindari Kontak Langsung, Benda PANAS");
+  }
+  if(value1 == 1){
+    digitalWrite(relay3, HIGH); // relay valve air panas ke mixer
+    Blynk.virtualWrite(V7, HIGH);
+    Blynk.virtualWrite(V0, HIGH);
+    delay(3000); // waktu ini butuh observasi lanjutan. Butuh berapa detik untuk setiap berapa ML. Soalnya air tidak boleh terlalu banyak karena bisa merusak adonan bahan briket
+    digitalWrite(relay3, LOW);
+    Blynk.virtualWrite(V7, LOW);
+    Blynk.virtualWrite(V0, LOW);
   }
 }
-BLYNK_WRITE(V4) {
-  bool value4 = param.asInt();
-  if (value4 == 1) {
-    digitalWrite(relay4, LOW);
-  } else {
-    digitalWrite(relay4, HIGH);
+
+BLYNK_WRITE(V8){
+  bool value5 = param.asInt();
+  if(value5 == 1){
+    digitalWrite(relay5, HIGH);
+  }else{
+    digitalWrite(relay5, LOW);
   }
 }
+
+// BLYNK_WRITE(V2) {
+//   bool value2 = param.asInt();
+//   if (value2 == 1) {
+//     digitalWrite(relay2, HIGH);
+//     // delay(10000); // Butuh riset, berapa ML air bisa didapatkan jika dalam waktu per 1 sec. Takutnya airnya kebanyakan saat diolah krn dpt merusak adonan briket.
+//     // digitalWrite(relay2, LOW);
+//     // Blynk.virtualWrite(V2, LOW);
+//   }
+//   else{
+//     digitalWrite(relay2, LOW);
+//     Blynk.virtualWrite(V2, LOW);
+//   }
+// }
 
 void setup()
 {
@@ -92,6 +112,7 @@ void setup()
   pinMode (relay2, OUTPUT);
   pinMode (relay3, OUTPUT);
   pinMode (relay4, OUTPUT);
+  pinMode (relay5, OUTPUT);
 
   // digitalWrite(relay1, HIGH);
   // digitalWrite(relay2, HIGH);
@@ -109,45 +130,29 @@ void loop()
   float s = thermocouple.readCelsius();
   Serial.print("C = ");
   Serial.println(thermocouple.readCelsius());
-  if(thermocouple.readCelsius() > 35.00){
-    digitalWrite(relay1, LOW);
+  if(thermocouple.readCelsius() > 65.00){ // Ini contoh suhu sementara, nanti bisa diubah menjadi 80c
+    digitalWrite(relay2, LOW);
+    Blynk.virtualWrite(V2, LOW);
+    Blynk.virtualWrite(V6, LOW);
     Blynk.virtualWrite(V0, LOW);
-    Blynk.virtualWrite(V5, LOW);
-    // Blynk.logEvent("suhu", "Air Panas Anda Sudah Siap");
+    Blynk.logEvent("suhu", "Air Panas Anda Sudah Siap"); // AKAN MENGIRIM NOTIFIKASI
+    // digitalWrite(relay3, HIGH);
+    // Blynk.virtualWrite(V7, HIGH);
+    // Blynk.virtualWrite(V0, HIGH);
+    // delay(3000);
+    // digitalWrite(relay3, LOW);
+    // Blynk.virtualWrite(V7, LOW);
+    // Blynk.virtualWrite(V0, LOW);
   }
-    // digitalWrite(relay1, HIGH);
-    // Blynk.logEvent("SUHU", "AIR Panas Anda Akan Siap Dalam Beberapa Menit Lagi. Mesin Akan Melanjutkan Proses Secara OTOMATIS");
-  // else{
-  //   digitalWrite(relay1, HIGH);
-  // }
-  // if(thermocouple.readCelsius() > 32.00){
-    // digitalWrite(relay2, LOW);
-  //   Blynk.logEvent("SUHU", "AIR Panas Anda Akan Siap Dalam Beberapa Menit Lagi. Mesin Akan Melanjutkan Proses Secara OTOMATIS");
-  // }
-  // else{
-  //   digitalWrite(relay2, HIGH);
-  // }
-  // if(thermocouple.readCelsius() > 38.00){
-  //   digitalWrite(relay3, LOW);
-  //   Blynk.logEvent("NOTIFIKASI...", "AIR Panas Anda Akan Siap Dalam Beberapa Menit Lagi. Mesin Akan Melanjutkan Proses Secara OTOMATIS");
-  // }else{
-  //   digitalWrite(relay3, HIGH);
-  // }
-  // if(thermocouple.readCelsius() > 42.00){
-  //   digitalWrite(relay4, LOW);
-  //   Blynk.logEvent("NOTIFIKASI...", "AIR Panas Anda Akan Siap Dalam Beberapa Menit Lagi. Mesin Akan Melanjutkan Proses Secara OTOMATIS");
-  // }else{
-  //   digitalWrite(relay4, HIGH);
-  // }
 
-  Serial.print("F = ");
+  Serial.print("F = ");// sebenarnya F tidak terlalu dibutuhkan karena sudah ada C
   Serial.println(thermocouple.readFahrenheit());
   delay(1000);
 
   Blynk.run();
   timer.run();
 }
-void sendSensor(){
+void sendSensor(){//syntax pengiriman value sensor
   float s = thermocouple.readCelsius();
   if(isnan(s)){
     Serial.println("FAILED");
